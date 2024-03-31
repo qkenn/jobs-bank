ccf9c4545a3235158f84a28524cb3ee5cf6f5449
Sithija Sandaruwan <noahsanchez@tutanota.com>
2024-03-31 15:07:47 +0530

    Added Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="wrapper">
+        <p className="footer__text">The site is made by sōsuke | 2024</p>
+      </div>
+    </footer>
+  );
+}
