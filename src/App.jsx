3cf6aa9ca03cd5ef9c475e239dee8d4b2172574b
Sithija Sandaruwan <noahsanchez@tutanota.com>
2024-03-31 15:45:02 +0530

    Added NotFoundPage

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ import {
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import MainLayout from "./layouts/MainLayout";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route path="/" element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
 );
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+  return (
+    <>
+      <section className="section">
+        <div className="error">
+          <h1 className="error__title">404 Not Found</h1>
+          <p className="error__description">this page does not exists</p>
+          <Link to="/" className="btn">
+            Go back
+          </Link>
+        </div>
+      </section>
+    </>
+  );
+}
